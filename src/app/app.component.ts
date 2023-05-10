b8cfb0c23a61cd3fa9c060dcfcbb0f659cca5d13
unknown <nikola.stanojevic@smartstream-stp.com>
2023-05-10 11:08:17 +0200

    project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-core';
+  numbers = [ 2,3,4,5];
+  numbersObs = of([ 2,3,4,5]);
+  response!: Observable<any> ;
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+      this.response = this.http.get('http://localhost:8080/user/username', {responseType: 'text'})
+  }
 }
